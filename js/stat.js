7cc6b62bd69c69082f26a8d00cb550dff512b2fb
Andrew <8911102@gmail.com>
2020-01-25 18:02:53 +0500

    times of players were added to show on cloud of statistics

--- js/stat.js
+++ js/stat.js
@@ -8,19 +8,84 @@ var CLOUD_Y = 10;
 var GAP = 10;
 var MAX_HEIGHT = 150;
 
-var COLUMN_Y = 200;
 var COLUMN_WIDTH = 40;
 
 var START_X = 130;
-var START_Y = 260;
+var START_Y = 265;
 
-var COLUMN_DISTANCE = 80;
+var COLUMN_DISTANCE = 100;
+var UPPER_GAP_COLUMN = 35;
 
+var namePlayers = ['Вы', 'Кекс', 'Катя', 'Игорь'];
+var timesPlayers = [2725, 4025, 1244, 1339];
+
+var colorUser = 'rgba(255, 0, 0, 1)';
+
+var listCalculatedHeight = []; // array with height of columns in statistic
+
+/*
+* find maximum in arr
+* @param {Object} arr - list of player's times.
+*/
+function getMax(arr) {
+  return arr.reduce(function (a, b) {
+    return Math.max(a, b);
+  });
+}
 
 var renderCloud = function (ctx, x, y, color) {
   ctx.fillStyle = color;
   ctx.fillRect(x, y, CLOUD_WIDTH, CLOUD_HEIGHT);
 };
+
+// show name of Players under columns
+// but not more than first 4 players
+function positionNames(names, ctx) {
+  for (var i = 0; i < names.length && i < 4; i++) {
+    ctx.fillText(names[i], START_X + COLUMN_DISTANCE * i, START_Y);
+  }
+}
+
+// returned random integer
+function getRandomIntInclusive(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min; // The maximum is inclusive and the minimum is inclusive
+}
+
+/*
+* show result of statistics
+* but not more than first 4 players
+* @param {Object} ctx - CanvasRenderingContext2D.
+* @param {Object} times - array of player's times.
+* */
+function showStatistic(ctx, times) {
+  var maxTime = getMax(times);
+  var calculatedHigh;
+
+  for (var i = 0; i < times.length && i < 4; i++) {
+    if (i === 0) {
+      ctx.fillStyle = colorUser;
+    } else {
+      ctx.fillStyle = `hsl(240, 100%, ${getRandomIntInclusive(10, 90)}%)`;
+    }
+    calculatedHigh = Math.floor((MAX_HEIGHT * times[i]) / maxTime);
+
+    ctx.fillRect(-(START_X + COLUMN_DISTANCE * i), 0, -COLUMN_WIDTH, calculatedHigh);
+    listCalculatedHeight.push(calculatedHigh);
+  }
+}
+/*
+* show times of players
+* but not more than first 4 players
+* @param {Object} ctx - CanvasRenderingContext2D.
+* @param {Object} times - array of player's times.
+* */
+function showTimesPlayers(ctx, times) {
+  for (var i = 0; i < times.length && i < 4; i++) {
+    ctx.fillText(times[i], START_X + COLUMN_DISTANCE * i, CLOUD_HEIGHT - listCalculatedHeight[i] - UPPER_GAP_COLUMN);
+  }
+}
 
 window.renderStatistics = function (ctx) {
   renderCloud(ctx, CLOUD_X + GAP, CLOUD_Y + GAP, 'rgba(0, 0, 0, 0.7 )');
@@ -33,19 +98,15 @@ window.renderStatistics = function (ctx) {
   ctx.fillText(lineTitle1, 120, 40);
   ctx.fillText(lineTitle2, 120, 60);
 
-  ctx.fillText('Вы', START_X + COLUMN_DISTANCE * 0, START_Y);
-  ctx.fillText('Иван', START_X + COLUMN_DISTANCE * 1, START_Y);
-  ctx.fillText('Юлия', START_X + COLUMN_DISTANCE * 2, START_Y);
-
-  ctx.translate(0, 230);
+  positionNames(namePlayers, ctx);
+  ctx.save();
+  // shift and rotate coordinate system
+  ctx.translate(0, 240);
   ctx.rotate(Math.PI);
-  ctx.fillStyle = 'rgba(255, 0, 0, 0.2)';
-  ctx.fillRect(-START_X + COLUMN_DISTANCE * 0, 0, -COLUMN_WIDTH, 100);
 
-  ctx.setTransform(1, 0, 0, 1, 0, 0);
+  showStatistic(ctx, timesPlayers);
 
-  ctx.fillStyle = 'hsl(240, 100%, 70%)';
-  ctx.fillRect(START_X + COLUMN_DISTANCE, COLUMN_Y, COLUMN_WIDTH, 20);
-
-  ctx.fillRect(START_X + COLUMN_DISTANCE * 2, COLUMN_Y, COLUMN_WIDTH, 20);
+  // set coordinates system to origin state
+  ctx.restore();
+  showTimesPlayers(ctx, timesPlayers);
 };
